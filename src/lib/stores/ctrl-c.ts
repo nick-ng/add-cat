69eca11a0913449950e2b73471a8cd1f93842359
nick-ng <nick-ng@users.noreply.github.com>
2025-08-28 22:45:18 +1200

    wip

--- src/lib/stores/ctrl-c.ts
+++ src/lib/stores/ctrl-c.ts
@@ -1,19 +1,20 @@
-import type {CtrlCStore} from '$lib/types'
+import type { CtrlCStore } from '$lib/types';
 
 import { writable } from 'svelte/store';
 import { browser } from '$app/environment';
-import {ctrlCSetSchema, ctrlCStoreSchema} from '$lib/types'
+import { ctrlCSetSchema, ctrlCStoreSchema } from '$lib/types';
 
-const STORE_PREFIX = "PUX_CTRL_C_SET_"
-const STORE_KEY = "PUX_CTRL_C_SETTINGS"
+const STORE_PREFIX = 'PUX_CTRL_C_SET_';
+const STORE_KEY = 'PUX_CTRL_C_SETTINGS';
 
 export const ctrlCStore = writable<CtrlCStore>({
-		default: "default",
-sets: { default: {name: "default", favourites: {}, refreshMs: 1000 * 60 * 60 * 24, strings: {}}
-		}
-})
+	default: 'default',
+	sets: {
+		default: { name: 'default', favourites: {}, refreshMs: 1000 * 60 * 60 * 24, strings: {} }
+	}
+});
 
 if (browser) {
-const stores = localStorage
-		console.log('stores', Object.keys(stores))
+	const stores = localStorage;
+	console.log('stores', Object.keys(stores));
 }
--- src/routes/ctrl-c/+page.svelte
+++ src/routes/ctrl-c/+page.svelte
@@ -1,62 +1,64 @@
 <script lang="ts">
-import {ctrlCStore} from '$lib/stores/ctrl-c'
+	import { ctrlCStore } from '$lib/stores/ctrl-c';
 
-let selectedSetName = $state($ctrlCStore.default)
-let currentSet = $derived($ctrlCStore.sets[selectedSetName])
+	let selectedSetName = $state($ctrlCStore.default);
+	let currentSet = $derived($ctrlCStore.sets[selectedSetName]);
 </script>
 
 <div class="container">
-<div class="controls">
+	<div class="controls">
 		<select bind:value={selectedSetName}>
-				{#each Object.values($ctrlCStore.sets) as ctrlCSet (ctrlCSet.name)}
-						<option value={ctrlCSet.name}>{ctrlCSet.name}</option>
-{/each}
-				</select>
-				<button type="button" onclick={() => {
-						let setName = prompt("Enter set name")
-						if (!setName) {
-								return
-						}
+			{#each Object.values($ctrlCStore.sets) as ctrlCSet (ctrlCSet.name)}
+				<option value={ctrlCSet.name}>{ctrlCSet.name}</option>
+			{/each}
+		</select>
+		<button
+			type="button"
+			onclick={() => {
+				let setName = prompt('Enter set name');
+				if (!setName) {
+					return;
+				}
 
-						setName = setName.toLocaleLowerCase().replaceAll(/[^a-z0-9_\- ]/g,"")
+				setName = setName.toLocaleLowerCase().replaceAll(/[^a-z0-9_\- ]/g, '');
 
-						if (!$ctrlCStore.sets[setName]) {
-								$ctrlCStore.sets[setName] = {
-										name: setName,
-										favourites: {},
-										refreshMs: 1000 * 60 * 60 * 24,
-										strings: {}
-								}
-						}
+				if (!$ctrlCStore.sets[setName]) {
+					$ctrlCStore.sets[setName] = {
+						name: setName,
+						favourites: {},
+						refreshMs: 1000 * 60 * 60 * 24,
+						strings: {}
+					};
+				}
 
-						selectedSetName = setName
-				}}>New Set</button>
-				<div style="flex-grow: 1;"></div>
-				<button type="button" onclick={() => {
-						}}>Import</button>
-				</div>
-		<div class="main">
-				<h2>{currentSet.name}</h2>
-				{#each Object.keys(currentSet.strings) as copyStringKey (copyStringKey)}
+				selectedSetName = setName;
+			}}>New Set</button
+		>
+		<div style="flex-grow: 1;"></div>
+		<button type="button" onclick={() => {}}>Import</button>
+	</div>
+	<div class="main">
+		<h2>{currentSet.name}</h2>
+		{#each Object.keys(currentSet.strings) as copyStringKey (copyStringKey)}
 			<div>{currentSet.strings[copyStringKey].history[0]}</div>
-				{/each}
-				</div>
+		{/each}
+	</div>
 </div>
 
 <style>
-.controls {
+	.controls {
 		display: flex;
 		flex-direction: row;
 		gap: 5px;
 
 		select {
-text-transform: capitalize;
+			text-transform: capitalize;
 		}
-}
+	}
 
-.main {
+	.main {
 		h2 {
-				text-transform: capitalize;
+			text-transform: capitalize;
 		}
-}
+	}
 </style>
