315673882692d39b16f6658bbe5bf6b144001a21
nick-ng <nick-ng@users.noreply.github.com>
2025-08-28 22:11:52 +1200

    wip

---
+++ src/lib/stores/ctrl-c.ts
@@ -0,0 +1,19 @@
+import type {CtrlCStore} from '$lib/types'
+
+import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
+import {ctrlCSetSchema, ctrlCStoreSchema} from '$lib/types'
+
+const STORE_PREFIX = "PUX_CTRL_C_SET_"
+const STORE_KEY = "PUX_CTRL_C_SETTINGS"
+
+export const ctrlCStore = writable<CtrlCStore>({
+		default: "default",
+sets: { default: {name: "default", favourites: {}, refreshMs: 1000 * 60 * 60 * 24, strings: {}}
+		}
+})
+
+if (browser) {
+const stores = localStorage
+		console.log('stores', Object.keys(stores))
+}
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,5 @@
+import z from 'zod';
+
 export type Doodad = {
 	x: number;
 	y: number;
@@ -17,3 +19,23 @@ export type Hideout = {
 	uploadTimestampMs: number;
 	doodadCounts: { [key: string]: number };
 };
+
+export const ctrlCSetSchema = z.object({
+	name: z.string(),
+	url: z.string().optional(),
+	refreshMs: z.number().default(1000 * 60 * 60 * 24), // 1 day in ms
+	favourites: z.record(z.number(), z.string()),
+	strings: z.record(z.string(),
+				z.object({
+						history: z.array(z.string()),
+						comment: z.string(),
+				})
+		)
+});
+
+export const ctrlCStoreSchema = z.object({
+	default: z.string(),
+	sets: z.record(z.string(), ctrlCSetSchema)
+});
+
+export type CtrlCStore = z.infer<typeof ctrlCStoreSchema>
