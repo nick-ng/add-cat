bc836bfaf190c4649642e9e96fe020d9f264836d
nick-ng <nick-ng@users.noreply.github.com>
2025-09-16 23:05:17 +1200

    feat: add a page to store strings you can copy to clipboard

--- src/lib/stores/ctrl-c.ts
+++ src/lib/stores/ctrl-c.ts
@@ -2,19 +2,76 @@ import type { CtrlCStore } from '$lib/types';
 
 import { writable } from 'svelte/store';
 import { browser } from '$app/environment';
-import { ctrlCSetSchema, ctrlCStoreSchema } from '$lib/types';
+import { ctrlCSetSchema } from '$lib/types';
 
 const STORE_PREFIX = 'PUX_CTRL_C_SET_';
-const STORE_KEY = 'PUX_CTRL_C_SETTINGS';
+// const STORE_KEY = 'PUX_CTRL_C_SETTINGS';
 
+// the default set has the key "d00". others will have "s<timestamp>"
 export const ctrlCStore = writable<CtrlCStore>({
-	default: 'default',
-	sets: {
-		default: { name: 'default', favourites: {}, refreshMs: 1000 * 60 * 60 * 24, strings: {} }
-	}
+	groups: {
+		d00: {
+			key: 'd00',
+			name: 'default',
+			defaultAt: 1,
+			favourites: {},
+			refreshMs: 1000 * 60 * 60 * 24,
+			strings: {}
+		}
+	},
+	updatedGroup: ''
 });
 
 if (browser) {
-	const stores = localStorage;
-	console.log('stores', Object.keys(stores));
+	const ctrlCSetKeys = Object.keys(localStorage).filter((k) => k.startsWith(STORE_PREFIX));
+	ctrlCStore.update((prev) => {
+		for (let i = 0; i < ctrlCSetKeys.length; i++) {
+			const key = ctrlCSetKeys[i];
+			const setString = localStorage.getItem(key);
+			if (!setString) {
+				continue;
+			}
+
+			try {
+				const unknown = JSON.parse(setString);
+				const result = ctrlCSetSchema.safeParse(unknown);
+				if (result.error) {
+					console.error('error when parsing set a', key, result.data);
+					continue;
+				}
+
+				const ctrlCSet = result.data;
+				prev.groups[ctrlCSet.key] = ctrlCSet;
+				console.log('ctrlCSet', i, ctrlCSet);
+			} catch (e) {
+				console.error('error when parsing set b', key, e);
+			}
+		}
+
+		return prev;
+	});
 }
+
+ctrlCStore.subscribe((nextData) => {
+	console.log(new Date());
+	const updatedGroup = nextData.groups[nextData.updatedGroup];
+	if (!updatedGroup) {
+		return;
+	}
+
+	const storeKey = `${STORE_PREFIX}${updatedGroup.key}`;
+	localStorage.setItem(storeKey, JSON.stringify(updatedGroup));
+});
+
+export const getDefault = (store: CtrlCStore) => {
+	let d = '';
+	let defaultTime = 0;
+	Object.values(store.groups).forEach((s) => {
+		if (s.defaultAt > defaultTime) {
+			d = s.key;
+			defaultTime = s.defaultAt;
+		}
+	});
+
+	return d;
+};
--- src/routes/ctrl-c/+page.svelte
+++ src/routes/ctrl-c/+page.svelte
@@ -1,51 +1,200 @@
 <script lang="ts">
-	import { ctrlCStore } from '$lib/stores/ctrl-c';
+	import { ctrlCStore, getDefault } from '$lib/stores/ctrl-c';
+	import { onMount } from 'svelte';
 
-	let selectedSetName = $state($ctrlCStore.default);
-	let currentSet = $derived($ctrlCStore.sets[selectedSetName]);
+	let selectedGroupKey = $state(getDefault($ctrlCStore));
+	let newString = $state('');
+	let newComment = $state('');
+	let editingString = $state('');
+
+	$effect(() => {
+		$ctrlCStore.updatedGroup = selectedGroupKey;
+	});
+
+	onMount(() => {
+		const keypressHandler = (event: KeyboardEvent) => {
+			if (!event.altKey || !event.ctrlKey) {
+				return;
+			}
+
+			const stringKey = $ctrlCStore.groups[selectedGroupKey].favourites[event.key];
+			if (!stringKey) {
+				return;
+			}
+
+			const aaaa = $ctrlCStore.groups[selectedGroupKey].strings[stringKey];
+
+			if (navigator?.clipboard) {
+				navigator.clipboard.writeText(aaaa.history[0]);
+			}
+		};
+
+		document.addEventListener('keyup', keypressHandler);
+
+		return () => {
+			document.removeEventListener('keyup', keypressHandler);
+		};
+	});
 </script>
+
+<svelte:head>
+	<title>PathOfExileSearchStrings</title>
+</svelte:head>
 
 <div class="container">
 	<div class="controls">
-		<select bind:value={selectedSetName}>
-			{#each Object.values($ctrlCStore.sets) as ctrlCSet (ctrlCSet.name)}
-				<option value={ctrlCSet.name}>{ctrlCSet.name}</option>
+		<select bind:value={selectedGroupKey}>
+			{#each Object.values($ctrlCStore.groups) as ctrlCSet (ctrlCSet.key)}
+				<option value={ctrlCSet.key}>{ctrlCSet.name}</option>
 			{/each}
 		</select>
 		<button
 			type="button"
 			onclick={() => {
-				let setName = prompt('Enter set name');
-				if (!setName) {
+				let groupName = prompt('Enter set name');
+				if (!groupName) {
 					return;
 				}
 
-				setName = setName.toLocaleLowerCase().replaceAll(/[^a-z0-9_\- ]/g, '');
-
-				if (!$ctrlCStore.sets[setName]) {
-					$ctrlCStore.sets[setName] = {
-						name: setName,
+				groupName = groupName.toLocaleLowerCase().replaceAll(/[^a-z0-9_\- ]/g, '');
+				const setKey = `s${Date.now()}`;
+				if (!$ctrlCStore.groups[groupName]) {
+					$ctrlCStore.groups[groupName] = {
+						name: groupName,
+						key: setKey,
+						defaultAt: 0,
 						favourites: {},
 						refreshMs: 1000 * 60 * 60 * 24,
 						strings: {}
 					};
 				}
 
-				selectedSetName = setName;
+				selectedGroupKey = setKey;
 			}}>New Set</button
 		>
 		<div style="flex-grow: 1;"></div>
-		<button type="button" onclick={() => {}}>Import</button>
+		<button
+			type="button"
+			onclick={() => {
+				alert('coming soon');
+			}}>Import</button
+		>
 	</div>
 	<div class="main">
-		<h2>{currentSet.name}</h2>
-		{#each Object.keys(currentSet.strings) as copyStringKey (copyStringKey)}
-			<div>{currentSet.strings[copyStringKey].history[0]}</div>
-		{/each}
+		<h2>{$ctrlCStore.groups[selectedGroupKey].name}</h2>
+		<h3>Favourites</h3>
+		<p>Press <code>CTRL + ALT + #</code> to copy the string to clipboard.</p>
+		<table class="copy-table">
+			<tbody>
+				{#each [1, 2, 3, 4, 5] as index (index)}
+					<tr>
+						<td style="text-align:right;">{index}</td>
+						<td
+							><select bind:value={$ctrlCStore.groups[selectedGroupKey].favourites[`${index}`]}>
+								<option value="">Choose a string</option>
+								{#each Object.keys($ctrlCStore.groups[selectedGroupKey].strings) as copyStringKey (copyStringKey)}
+									<option value={copyStringKey}
+										>{$ctrlCStore.groups[selectedGroupKey].strings[copyStringKey]
+											.history[0]}</option
+									>
+								{/each}</select
+							>
+						</td>
+						<td style="text-align:right;padding-left:8px;">{index + 5}</td>
+						<td
+							><select bind:value={$ctrlCStore.groups[selectedGroupKey].favourites[`${index + 5}`]}>
+								<option value="">Choose a string</option>
+								{#each Object.keys($ctrlCStore.groups[selectedGroupKey].strings) as copyStringKey (copyStringKey)}
+									<option value={copyStringKey}
+										>{$ctrlCStore.groups[selectedGroupKey].strings[copyStringKey]
+											.history[0]}</option
+									>
+								{/each}</select
+							>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+		<div>
+			<details>
+				<summary>Add String</summary>
+				<form
+					onsubmit={(e) => {
+						e.preventDefault();
+
+						const stringKey = `c${Date.now()}`;
+						$ctrlCStore.groups[selectedGroupKey].strings[stringKey] = {
+							history: [newString],
+							comment: newComment
+						};
+
+						newString = '';
+						newComment = '';
+						editingString = '';
+					}}
+				>
+					<label>String <input type="text" bind:value={newString} /></label>
+					<button type="submit">Add</button>
+				</form>
+			</details>
+		</div>
+		<table class="copy-table">
+			<tbody>
+				{#each Object.keys($ctrlCStore.groups[selectedGroupKey].strings) as copyStringKey (copyStringKey)}
+					<tr>
+						<td
+							><button
+								type="button"
+								onclick={() => {
+									if (navigator?.clipboard) {
+										navigator.clipboard.writeText(
+											$ctrlCStore.groups[selectedGroupKey].strings[copyStringKey].history[0]
+										);
+									}
+								}}>Copy</button
+							></td
+						>
+						<td>{$ctrlCStore.groups[selectedGroupKey].strings[copyStringKey].history[0]}</td>
+						<td class="inline"
+							><button
+								type="button"
+								onclick={() => {
+									editingString = copyStringKey;
+									newString =
+										$ctrlCStore.groups[selectedGroupKey].strings[copyStringKey].history[0];
+								}}>Edit</button
+							>
+							{#if editingString === copyStringKey}<form
+									onsubmit={(e) => {
+										e.preventDefault();
+
+										$ctrlCStore.groups[selectedGroupKey].strings[copyStringKey].history[0] =
+											newString;
+
+										editingString = '';
+									}}
+								>
+									<input bind:value={newString} /> <button>Done</button>
+								</form>{/if}</td
+						>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
 	</div>
 </div>
 
 <style>
+	summary,
+	button {
+		cursor: pointer;
+	}
+
+	.container {
+		font-family: sans-serif;
+	}
+
 	.controls {
 		display: flex;
 		flex-direction: row;
@@ -56,9 +205,19 @@
 		}
 	}
 
+	.copy-table {
+		td {
+			padding: 5px 2px;
+		}
+	}
+
 	.main {
 		h2 {
 			text-transform: capitalize;
 		}
+	}
+
+	.inline * {
+		display: inline;
 	}
 </style>
