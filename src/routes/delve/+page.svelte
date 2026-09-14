31d6977bc53a684b9f09f28d3aaf1fd97e000673
pux <nick-ng@users.noreply.github.com>
2026-09-14 18:46:06 +1200

    wip: remove unused imports from delve.

--- src/routes/delve/+page.svelte
+++ src/routes/delve/+page.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
-	import type { DelveRow } from '$lib/types';
 	import type { PageProps } from './+page';
-
-	import localforage from 'localforage';
-	import { delveRowsSchema, delveSessionsSchema } from '$lib/types';
-	import { requestPersistence } from '$lib/utils';
-	import { STORE_PREFIX, SESSIONS_STORE_KEY } from './constants';
 
 	let { data }: PageProps = $props();
 </script>
 
 <div class="w-prose2 m-auto my-2">
 	<h1>Delve</h1>
+	<p>Coming soon</p>
 	<details>
 		<summary> Debug </summary>
 		<pre>{JSON.stringify(data)}</pre>
